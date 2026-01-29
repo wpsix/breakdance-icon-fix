e438df954cbf75f34c8a3430ddbb0040f13e5480
Bence <gforceyakuzi@gmail.com>
2026-01-29 09:21:15 +0100

    first commit

---
+++ plugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Breakdance Icon Fix
+ * Plugin URI: https://wpsix.com/plugins/breakdance-icon-fix/
+ * Description: Fix for the Breakdance Icons plugin
+ * Version: 1.0.0
+ * Author: Bence Boruzs
+ * Author URI: https://wpsix.com/
+ * License: GPL2
+ * Text Domain: breakdance-icon-fix
+ */
+
+use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('breakdance_loaded', function () {
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder( __DIR__ ) . '/macros',
+        'BreakdanceIconFix',
+        'macro',
+        'Breakdance Icon Fix Macros',
+        true,
+    );
+}, 11 );
